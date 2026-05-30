ecbca57ceadd636d34c95a83f85a9467a344eeda
tharikadheena-ui <tharikadheena@gmail.com>
2026-05-30 14:20:09 +0530

    backend server setup with mongodb connection

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({message: 'Zen API is running!'});
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+   console.log(`ZEN server running on port ${PORT}`);;
+});
